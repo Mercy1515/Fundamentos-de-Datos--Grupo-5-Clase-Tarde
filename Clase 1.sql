2498db6338c7c99834aa1d9acf74f84dfa2eb761
Mercy1515 <mercevasquez15@gmail.com>
2026-07-15 16:04:14 -0600

    Add files via upload

---
+++ Clase 1.sql
@@ -0,0 +1,22 @@
+create database clase1
+go
+
+use clase1
+go
+
+create table usuario(
+id int,
+direc varchar(30),
+telefono int
+
+)
+go
+
+create table producto (
+id int not null,
+nombre varchar (20) not null,
+precio int not null 
+)
+go
+
+
